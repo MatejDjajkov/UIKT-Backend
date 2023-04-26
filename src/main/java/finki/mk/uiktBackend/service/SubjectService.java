4da261bb08c910e7fd0c8e39fefab3ede22b3471
MatejDjajkov <dzajkov72@gmail.com>
2023-04-26 15:50:29 +0200

    Added basic filters so we can filter Subjects by semester Type,Module, Professor and Year

--- src/main/java/finki/mk/uiktBackend/service/SubjectService.java
+++ src/main/java/finki/mk/uiktBackend/service/SubjectService.java
@@ -1,9 +1,19 @@
 package finki.mk.uiktBackend.service;
 
 import finki.mk.uiktBackend.model.Subject;
+import finki.mk.uiktBackend.model.enums.SemesterType;
+import finki.mk.uiktBackend.model.enums.Year;
 
 import java.util.List;
 
 public interface SubjectService {
     List<Subject> getAllSubjects();
+
+    List<Subject> findAllByYear(Year year);
+
+    List<Subject> findAllBySemester(SemesterType semesterType);
+
+    List<Subject> findAllByModule(String moduleName);
+
+    List<Subject> findAllByProfessorName(String professorName);
 }
--- src/main/java/finki/mk/uiktBackend/service/impl/SubjectServiceImpl.java
+++ src/main/java/finki/mk/uiktBackend/service/impl/SubjectServiceImpl.java
@@ -1,6 +1,8 @@
 package finki.mk.uiktBackend.service.impl;
 
 import finki.mk.uiktBackend.model.Subject;
+import finki.mk.uiktBackend.model.enums.SemesterType;
+import finki.mk.uiktBackend.model.enums.Year;
 import finki.mk.uiktBackend.repository.SubjectRepository;
 import finki.mk.uiktBackend.service.SubjectService;
 import lombok.AllArgsConstructor;
@@ -16,4 +18,26 @@ public class SubjectServiceImpl implements SubjectService {
     public List<Subject> getAllSubjects() {
         return subjectRepository.findAll();
     }
+
+    @Override
+    public List<Subject> findAllByYear(Year year) {
+        return subjectRepository.findAllByYear(year);
+    }
+
+    @Override
+    public List<Subject> findAllBySemester(SemesterType semesterType) {
+        return subjectRepository.findAllBySemesterType(semesterType);
+    }
+
+    @Override
+    public List<Subject> findAllByModule(String moduleName) {
+        return subjectRepository.findAllByModuleName(moduleName);
+    }
+
+    @Override
+    public List<Subject> findAllByProfessorName(String professorName) {
+        return subjectRepository.findByProfessorName(professorName);
+    }
+
+
 }
