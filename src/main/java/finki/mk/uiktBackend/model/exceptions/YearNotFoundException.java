6b54faa4073ae845737543fbf32e45c69a82bee6
MatejDjajkov <dzajkov72@gmail.com>
2023-04-26 17:02:24 +0200

    Added exceptions

---
+++ src/main/java/finki/mk/uiktBackend/model/exceptions/YearNotFoundException.java
@@ -0,0 +1,11 @@
+package finki.mk.uiktBackend.model.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus (code = HttpStatus.NOT_FOUND)
+public class YearNotFoundException extends RuntimeException{
+    public YearNotFoundException(){
+        super("Year not found");
+    }
+}
